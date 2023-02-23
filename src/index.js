fc9b9e80bed495b60e4b710f26f478417096cb81
beloyosry <byosry@gmail.com>
2023-02-23 22:07:03 +0200

    most sold

--- src/index.js
+++ src/index.js
@@ -8,4 +8,8 @@ import "@fortawesome/fontawesome-free/js/all";
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+
+    $(".add-to-cart-btn").click(function () {
+        alert("تم إضافة المنتج إلى عربة الشراء");
+    });
 });
